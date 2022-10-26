e2d4187a2c1fa59b10a027cdd0c40b1b1b0c0e19
Yavuz Avci <yavuzavci01@gmail.com>
2022-10-26 15:26:15 +0300

    Uygulama bitti

--- script.js
+++ script.js
@@ -2,19 +2,47 @@ const form = document.querySelector('form');
 const input = document.querySelector('#txtTaskName');
 const btnDeleteAll = document.querySelector('#btnDeleteAll');
 const taskList = document.querySelector('#task-list');
+let items;
 eventListeners();
+loadItems();
 
 function eventListeners() {
     form.addEventListener('submit', addNewItem);
-}
-
-function addNewItem(e) {
-    if (input.value === '') {
-        alert('Add New Item Please')
+    taskList.addEventListener('click', deleteItem)
+    btnDeleteAll.addEventListener('click', deleteAllItem)
+};
+function loadItems() {
+    items=getItemsFromLS();
+    items.forEach(function(item){
+        createItem(item);
+    })
+};
+function getItemsFromLS(){
+    if(localStorage.getItem('items')===null){
+        items=[];
+    }else{
+        items=JSON.parse(localStorage.getItem('items'))
     }
+    return items;
+};
+function setItemToLS(text){
+    items=getItemsFromLS();
+    items.push(text);
+    localStorage.setItem('items',JSON.stringify(items));
+};
+function deleteItemFromLS(text){
+    items=getItemsFromLS();
+    items.forEach(function(item,index){
+        if(item===text){
+            items.splice(index,1);
+        }
+    });
+    localStorage.setItem('items', JSON.stringify(items));
+};
+function createItem(text) {
     const li = document.createElement('li');
     li.className = 'list-group-item list-group-item-secondary';
-    li.appendChild(document.createTextNode(input.value));
+    li.appendChild(document.createTextNode(text));
     const a = document.createElement('a');
     a.classList = ('delete-item float-right');
     a.setAttribute('href', '#');
@@ -22,7 +50,37 @@ function addNewItem(e) {
     li.appendChild(a);
     taskList.appendChild(li);
     console.log(li);
+};
+function addNewItem(e) {
+    if (input.value === '') {
+        alert('Add New Item Please')
+    }
+    createItem(input.value);
+    setItemToLS(input.value);
     input.value = "";
-    // input.value("");
     e.preventDefault();
-}
+};
+function deleteItem(e) {
+    if (e.target.className === "fas fa-times") {
+            if (confirm('Are You Sure ?')) {
+            e.target.parentElement.parentElement.remove();
+            deleteItemFromLS(e.target.parentElement.parentElement.textContent);
+        }
+    }
+    e.preventDefault();
+};
+function deleteAllItem(e) {
+    if (confirm('Are You Sure ?')) {
+        // taskList.innerHTML = '';
+        // taskList.childNodes.forEach(function (item) {
+        //     if (item.nodeType === 1) {
+        //         item.remove();
+        //     }
+        // });
+        while (taskList.firstChild) {
+            taskList.removeChild(taskList.firstChild);
+        }
+        localStorage.clear();
+    }
+    e.preventDefault();
+};
